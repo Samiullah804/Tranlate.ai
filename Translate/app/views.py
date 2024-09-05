5ecb3320c1661d80038f3ff7642e3765f691706b
Samiullah804 <samiullahsmiles@gmail.com>
2024-09-05 15:30:38 +0500

    'new'

--- Translate/app/views.py
+++ Translate/app/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def chatView(request):
+    if request.method == "POST":
+        message = request.POST.get('message')
+        print(message)
     return render(request, "app/chat.html")
