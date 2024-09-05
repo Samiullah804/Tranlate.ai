3d3912536155a714186550f1f2725536cf0d9a3e
Samiullah804 <samiullahsmiles@gmail.com>
2024-09-05 16:04:53 +0500

    'updated_view'

--- Translate/app/views.py
+++ Translate/app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from .chatbot import Chatwithgroq
-
+from django.http import JsonResponse
 
 def chatView(request):
     if request.method == "POST":
         message = request.POST.get('message')
         response = Chatwithgroq(message)
+        response = JsonResponse({"response": response})
+        return response
 
     return render(request, "app/chat.html")
